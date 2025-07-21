5111e9cc08efcd3318f229ff85325968ce0fae9d
work <137403706+AltheaVestrit@users.noreply.github.com>
2025-07-21 13:20:16 +0200

    practice modules

---
+++ modules/one.js
@@ -0,0 +1,8 @@
+// one.js
+export const greeting = "Hello, Odinite!";
+export const farewell = "Bye bye, Odinite!";
+
+const generalGreeting = "Hello!";
+const generalFarewell = "Bye bye!";
+
+export { generalGreeting, generalFarewell };
---
+++ modules/two.js
@@ -0,0 +1,4 @@
+import { greeting, generalFarewell } from "./one.js";
+
+console.log(greeting);
+console.log(generalFarewell);
